935b2f1a98f47085d26bb62b43d1fcc6ea4cd4f6
Titash-2807 <155870248+Titash-shil@users.noreply.github.com>
2025-01-16 00:43:28 +0530

    Like. comment, share & don't forget to Subscribe to {https://youtube.com/@titashshil?si=g0znCfbVs5zRle1D}(Qwiklab_Explorers_TS)

--- qwiklab_explorers_gsp1073.sh
+++ qwiklab_explorers_gsp1073.sh
@@ -1,3 +1,30 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution...${RESET}"
+
 cat > deidentify-template.json <<'EOF_END'
 {
   "deidentifyTemplate": {
@@ -46,6 +73,233 @@ cat > deidentify-template.json <<'EOF_END'
               { "name": "vin" },
               { "name": "id" },
               { "name": "agent_id" },
+              { "name": "user_id" }
+            ],
+            "primitiveTransformation": {
+              "replaceConfig": {
+                "newValue": {
+                  "stringValue": "[redacted]"
+                }
+              }
+            }
+          }
+        ]
+      }
+    },
+    "displayName": "deid_struct1 template"
+  },
+  "templateId": "deid_struct1",
+  "locationId": "global"
+}
+EOF_END
+
+# Make the API call to create the second deidentify template
+echo "${CYAN}${BOLD}Creating deidentify template with Google Cloud DLP API...${RESET}"
+curl -s \
+-H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
+-H "Content-Type: application/json" \
+https://dlp.googleapis.com/v2/projects/$DEVSHELL_PROJECT_ID/deidentifyTemplates \
+-d @deidentify-template.json
+
+cat > job-configuration.json << EOM
+{
+  "triggerId": "DeID_Storage_Demo1",
+  "jobTrigger": {
+    "triggers": [
+      {
+        "schedule": {
+          "recurrencePeriodDuration": "604800s"
+        }
+      }
+    ],
+    "inspectJob": {
+      "actions": [
+        {
+          "deidentify": {
+            "fileTypesToTransform": [
+              "TEXT_FILE",
+              "IMAGE",
+              "CSV",
+              "TSV"
+            ],
+            "transformationDetailsStorageConfig": {},
+            "transformationConfig": {
+              "deidentifyTemplate": "projects/$DEVSHELL_PROJECT_ID/locations/global/deidentifyTemplates/deid_unstruct1",
+              "structuredDeidentifyTemplate": "projects/$DEVSHELL_PROJECT_ID/locations/global/deidentifyTemplates/deid_struct1"
+            },
+            "cloudStorageOutput": "gs://$DEVSHELL_PROJECT_ID-output"
+          }
+        }
+      ],
+      "inspectConfig": {
+        "infoTypes": [
+          {
+            "name": "ADVERTISING_ID"
+          },
+          {
+            "name": "AGE"
+          },
+          {
+            "name": "ARGENTINA_DNI_NUMBER"
+          },
+          {
+            "name": "AUSTRALIA_TAX_FILE_NUMBER"
+          },
+          {
+            "name": "BELGIUM_NATIONAL_ID_CARD_NUMBER"
+          },
+          {
+            "name": "BRAZIL_CPF_NUMBER"
+          },
+          {
+            "name": "CANADA_SOCIAL_INSURANCE_NUMBER"
+          },
+          {
+            "name": "CHILE_CDI_NUMBER"
+          },
+          {
+            "name": "CHINA_RESIDENT_ID_NUMBER"
+          },
+          {
+            "name": "COLOMBIA_CDC_NUMBER"
+          },
+          {
+            "name": "CREDIT_CARD_NUMBER"
+          },
+          {
+            "name": "CREDIT_CARD_TRACK_NUMBER"
+          },
+          {
+            "name": "DATE_OF_BIRTH"
+          },
+          {
+            "name": "DENMARK_CPR_NUMBER"
+          },
+          {
+            "name": "EMAIL_ADDRESS"
+          },
+          {
+            "name": "ETHNIC_GROUP"
+          },
+          {
+            "name": "FDA_CODE"
+          },
+          {
+            "name": "FINLAND_NATIONAL_ID_NUMBER"
+          },
+          {
+            "name": "FRANCE_CNI"
+          },
+          {
+            "name": "FRANCE_NIR"
+          },
+          {
+            "name": "FRANCE_TAX_IDENTIFICATION_NUMBER"
+          },
+          {
+            "name": "GENDER"
+          },
+          {
+            "name": "GERMANY_IDENTITY_CARD_NUMBER"
+          },
+          {
+            "name": "GERMANY_TAXPAYER_IDENTIFICATION_NUMBER"
+          },
+          {
+            "name": "HONG_KONG_ID_NUMBER"
+          },
+          {
+            "name": "IBAN_CODE"
+          },
+          {
+            "name": "IMEI_HARDWARE_ID"
+          },
+          {
+            "name": "INDIA_AADHAAR_INDIVIDUAL"
+          },
+          {
+            "name": "INDIA_GST_INDIVIDUAL"
+          },
+          {
+            "name": "INDIA_PAN_INDIVIDUAL"
+          },
+          {
+            "name": "INDONESIA_NIK_NUMBER"
+          },
+          {
+            "name": "IRELAND_PPSN"
+          },
+          {
+            "name": "ISRAEL_IDENTITY_CARD_NUMBER"
+          },
+          {
+            "name": "JAPAN_INDIVIDUAL_NUMBER"
+          },
+          {
+            "name": "KOREA_RRN"
+          },
+          {
+            "name": "MAC_ADDRESS"
+          },
+          {
+            "name": "MEXICO_CURP_NUMBER"
+          },
+          {
+            "name": "NETHERLANDS_BSN_NUMBER"
+          },
+          {
+            "name": "NORWAY_NI_NUMBER"
+          },
+          {
+            "name": "PARAGUAY_CIC_NUMBER"
+          },
+          {
+            "name": "PASSPORT"
+          },
+          {
+            "name": "PERSON_NAME"
+          },
+          {
+            "name": "PERU_DNI_NUMBER"
+          },
+          {
+            "name": "PHONE_NUMBER"
+          },
+          {
+            "name": "POLAND_NATIONAL_ID_NUMBER"
+          },
+          {
+            "name": "PORTUGAL_CDC_NUMBER"
+          },
+          {
+            "name": "SCOTLAND_COMMUNITY_HEALTH_INDEX_NUMBER"
+          },
+          {
+            "name": "SINGAPORE_NATIONAL_REGISTRATION_ID_NUMBER"
+          },
+          {
+            "name": "SPAIN_CIF_NUMBER"
+          },
+          {
+            "name": "SPAIN_DNI_NUMBER"
+          },
+          {
+            "name": "SPAIN_NIE_NUMBER"
+          },
+          {
+            "name": "SPAIN_NIF_NUMBER"
+          },
+          {
+            "name": "SPAIN_SOCIAL_SECURITY_NUMBER"
+          },
+          {
+            "name": "STORAGE_SIGNED_URL"
+          },
+          {
+            "name": "STREET_ADDRESS"
+          },
+          {
+            "name": "SWEDEN_NATIONAL_ID_NUMBER"
           },
           {
             "name": "SWIFT_CODE"
@@ -165,3 +419,8 @@ https://dlp.googleapis.com/v2/projects/$DEVSHELL_PROJECT_ID/locations/global/job
 # Provide link to deidentification template edit page
 echo "${MAGENTA}${BOLD}Deidentification Template Successfully Created! Edit the template here: ${RESET}"
 echo "https://console.cloud.google.com/security/sensitive-data-protection/projects/$DEVSHELL_PROJECT_ID/locations/global/deidentifyTemplates/deid_struct1/edit?project=$DEVSHELL_PROJECT_ID"
+
+# Congratulations message
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab!!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
