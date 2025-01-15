f64eb393dc32fc5313c7f6438e4f1bd3eca9e86c
Titash-2807 <155870248+Titash-shil@users.noreply.github.com>
2025-01-16 00:30:42 +0530

    Like. comment, share & don't forget to Subscribe to {https://youtube.com/@titashshil?si=g0znCfbVs5zRle1D}(Qwiklab_Explorers_TS)

---
+++ qwiklab_explorers_gsp1073.sh
@@ -0,0 +1,167 @@
+cat > deidentify-template.json <<'EOF_END'
+{
+  "deidentifyTemplate": {
+    "deidentifyConfig": {
+      "infoTypeTransformations": {
+        "transformations": [
+          {
+            "infoTypes": [
+              {
+                "name": ""
+              }
+            ],
+            "primitiveTransformation": {
+              "replaceWithInfoTypeConfig": {}
+            }
+          }
+        ]
+      }
+    },
+    "displayName": "deid_unstruct1 template"
+  },
+  "locationId": "global",
+  "templateId": "deid_unstruct1"
+}
+EOF_END
+
+# Make the API call to create the deidentify template
+echo "${CYAN}${BOLD}Creating deidentify template with Google Cloud DLP API...${RESET}"
+curl -s \
+-H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
+-H "Content-Type: application/json" \
+https://dlp.googleapis.com/v2/projects/$DEVSHELL_PROJECT_ID/deidentifyTemplates \
+-d @deidentify-template.json
+
+cat > deidentify-template.json <<'EOF_END'
+{
+  "deidentifyTemplate": {
+    "deidentifyConfig": {
+      "recordTransformations": {
+        "fieldTransformations": [
+          {
+            "fields": [
+              { "name": "ssn" },
+              { "name": "ccn" },
+              { "name": "email" },
+              { "name": "vin" },
+              { "name": "id" },
+              { "name": "agent_id" },
+          },
+          {
+            "name": "SWIFT_CODE"
+          },
+          {
+            "name": "THAILAND_NATIONAL_ID_NUMBER"
+          },
+          {
+            "name": "TURKEY_ID_NUMBER"
+          },
+          {
+            "name": "UK_NATIONAL_HEALTH_SERVICE_NUMBER"
+          },
+          {
+            "name": "UK_NATIONAL_INSURANCE_NUMBER"
+          },
+          {
+            "name": "UK_TAXPAYER_REFERENCE"
+          },
+          {
+            "name": "URUGUAY_CDI_NUMBER"
+          },
+          {
+            "name": "US_BANK_ROUTING_MICR"
+          },
+          {
+            "name": "US_EMPLOYER_IDENTIFICATION_NUMBER"
+          },
+          {
+            "name": "US_HEALTHCARE_NPI"
+          },
+          {
+            "name": "US_INDIVIDUAL_TAXPAYER_IDENTIFICATION_NUMBER"
+          },
+          {
+            "name": "US_SOCIAL_SECURITY_NUMBER"
+          },
+          {
+            "name": "VEHICLE_IDENTIFICATION_NUMBER"
+          },
+          {
+            "name": "VENEZUELA_CDI_NUMBER"
+          },
+          {
+            "name": "WEAK_PASSWORD_HASH"
+          },
+          {
+            "name": "AUTH_TOKEN"
+          },
+          {
+            "name": "AWS_CREDENTIALS"
+          },
+          {
+            "name": "AZURE_AUTH_TOKEN"
+          },
+          {
+            "name": "BASIC_AUTH_HEADER"
+          },
+          {
+            "name": "ENCRYPTION_KEY"
+          },
+          {
+            "name": "GCP_API_KEY"
+          },
+          {
+            "name": "GCP_CREDENTIALS"
+          },
+          {
+            "name": "JSON_WEB_TOKEN"
+          },
+          {
+            "name": "HTTP_COOKIE"
+          },
+          {
+            "name": "XSRF_TOKEN"
+          }
+        ],
+        "minLikelihood": "POSSIBLE"
+      },
+      "storageConfig": {
+        "cloudStorageOptions": {
+          "filesLimitPercent": 100,
+          "fileTypes": [
+            "TEXT_FILE",
+            "IMAGE",
+            "WORD",
+            "PDF",
+            "AVRO",
+            "CSV",
+            "TSV",
+            "EXCEL",
+            "POWERPOINT"
+          ],
+          "fileSet": {
+            "regexFileSet": {
+              "bucketName": "$DEVSHELL_PROJECT_ID-input",
+              "includeRegex": [],
+              "excludeRegex": []
+            }
+          }
+        }
+      }
+    },
+    "status": "HEALTHY"
+  }
+}
+EOM
+
+# Make the API call to create the job trigger
+echo "${CYAN}${BOLD}Creating job trigger for scheduled deidentification...${RESET}"
+curl -s \
+-H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
+-H "Content-Type: application/json" \
+https://dlp.googleapis.com/v2/projects/$DEVSHELL_PROJECT_ID/locations/global/jobTriggers \
+-d @job-configuration.json
+
+# Provide link to deidentification template edit page
+echo "${MAGENTA}${BOLD}Deidentification Template Successfully Created! Edit the template here: ${RESET}"
+echo "https://console.cloud.google.com/security/sensitive-data-protection/projects/$DEVSHELL_PROJECT_ID/locations/global/deidentifyTemplates/deid_struct1/edit?project=$DEVSHELL_PROJECT_ID"
